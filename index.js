e72a2aa24d260587e78b019c1bd520ab6d68c0a0
Vinicius Viera <viniciusviera301@gmail.com>
2023-11-20 11:25:30 -0300

    conexao com o mysql finalizada

--- index.js
+++ index.js
@@ -16,8 +16,19 @@ app.get('/', (requisicao, resposta) => {
 const conexao = mysql.createConnection({
     host: "localhost",
     user: "root",
-    passaword: "1234",
+    password: "1234",
     database: "todoapp",
     port: 3306
 })
 
+conexao.connect((erro) => {
+    if (erro) {
+        return console.log(erro)
+    }
+
+    console.log("Estou conectado ao MySQL")
+
+    app.listen(3000, () => {
+        console.log("Servidor rodando na porta 3000!")
+    })
+})
