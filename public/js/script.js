1a177cfc4eafa85b0945baac02d603f706f65170
Vinicius Viera <viniciusviera301@gmail.com>
2023-11-26 20:51:33 -0300

    Exclusão de tarefas individuais

--- public/js/script.js
+++ public/js/script.js
@@ -22,6 +22,16 @@ function descompletarTarefa(id) {
     window.location.reload()
 }
 
+function excluirTarefas(id) {
+    fetch("http://localhost:3000/excluir", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/Json'
+        },
+        body: JSON.stringify({ id })
+    })
+}
+
 function alterarTema() {
     const tema = localStorage.getItem("tema")
     const body = document.querySelector("body")
